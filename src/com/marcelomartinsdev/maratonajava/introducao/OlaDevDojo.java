2c63f209519b6fadf7a8d026df1edd535efa7456
Marcelo Martins <marcelomartinsdev@gmail.com>
2024-08-15 17:02:12 -0300

    feat: Initialize MaratoneJava course E0-10

---
+++ src/com/marcelomartinsdev/maratonajava/introducao/OlaDevDojo.java
@@ -0,0 +1,7 @@
+package com.marcelomartinsdev.maratonajava.introducao;
+
+public class OlaDevDojo {
+    public static void main(String[] args) {
+        System.out.println("Hello DevDojo");
+    }
+}
---
+++ src/com/marcelomartinsdev/maratonajava/introducao/TiposPrimitivos.java
@@ -0,0 +1,18 @@
+package com.marcelomartinsdev.maratonajava.introducao;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+
+        int idade = 10;
+        System.out.println("A idade e: "+idade+" anos");
+
+        double salario = 10000;
+        float salario2 = 10000;
+        char idadeChar = 'A';
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        long idadeLong = 10L;
+        boolean idadeBoolean = true;
+    }
+}
